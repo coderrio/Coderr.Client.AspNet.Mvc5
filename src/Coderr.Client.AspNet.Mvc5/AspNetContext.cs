e2dea07b117e9f740456c30fe28537372cd22313
jgauffin <jonas@gauffin.org>
2018-01-25 20:41:16 +0100

    updated to latest core package.

--- src/Coderr.Client.AspNet.Mvc5/AspNetContext.cs
+++ src/Coderr.Client.AspNet.Mvc5/AspNetContext.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web;
+using codeRR.Client.Contracts;
 using codeRR.Client.Reporters;
 
 namespace codeRR.Client.AspNet.Mvc5
 {
     /// <summary>
-    /// Reporter context with information about ASP.NET.
+    ///     Reporter context with information about ASP.NET.
     /// </summary>
-    public class AspNetContext : IErrorReporterContext
+    public class AspNetContext : IErrorReporterContext2
     {
         /// <summary>
-        /// Creates a new instance of <see cref="AspNetContext"/>.
+        ///     Creates a new instance of <see cref="AspNetContext" />.
         /// </summary>
         /// <param name="reporter">object triggering the collection</param>
         /// <param name="exception">caught exception</param>
@@ -20,25 +22,29 @@ namespace codeRR.Client.AspNet.Mvc5
         {
             if (reporter == null) throw new ArgumentNullException("reporter");
             if (exception == null) throw new ArgumentNullException("exception");
+            ContextCollections = new List<ContextCollectionDTO>();
             Exception = exception;
             HttpContext = httpContext;
             Reporter = reporter;
         }
 
         /// <summary>
-        /// The ASP.NET application 
+        ///     The ASP.NET application
         /// </summary>
         public HttpApplication HttpApplication { get; set; }
 
         /// <summary>
-        /// Http context for the request that the exception occurred for.
+        ///     Http context for the request that the exception occurred for.
         /// </summary>
         public HttpContextBase HttpContext { get; private set; }
 
-        /// <inheritdoc/>
+        /// <inheritdoc />
         public Exception Exception { get; private set; }
 
-        /// <inheritdoc/>
+        /// <inheritdoc />
         public object Reporter { get; private set; }
+
+        /// <inheritdoc />
+        public IList<ContextCollectionDTO> ContextCollections { get; }
     }
 }
